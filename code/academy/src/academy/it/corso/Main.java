0cc8d188f398c98c12556a02a97ccbefbbd5e1c4
Ale <alessandro.sallese@student.univaq.it>
2022-11-24 14:30:35 +0100

    Update 7

--- code/academy/src/academy/it/corso/Main.java
+++ code/academy/src/academy/it/corso/Main.java
@@ -1,11 +1,16 @@
 package academy.it.corso;
 
 public class Main {
+	
+	enum Level {
+		HIGHT,
+		LOW
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello \n");
-		System.out.println(1);
+		System.out.println(Level.HIGHT);
 
 	}
 
