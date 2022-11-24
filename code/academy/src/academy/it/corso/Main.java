a571620a96aab8eeb65a23f449a803c4bc707f3b
Alessandro <alex_994@ymail.com>
2022-11-24 13:35:10 +0100

    update 3

---
+++ code/academy/src/academy/it/corso/Main.java
@@ -0,0 +1,12 @@
+package academy.it.corso;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello \n");
+		System.out.println(1);
+
+	}
+
+}
