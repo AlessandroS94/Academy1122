7fbb46dc8d3bf5c4e9be63ea79e0fad6fc839a59
Alessandro <alex_994@ymail.com>
2022-11-25 10:24:13 +0100

    update slide and code

--- code/academy/src/academy/it/corso/Main.java
+++ code/academy/src/academy/it/corso/Main.java
@@ -2,16 +2,26 @@ package academy.it.corso;
 
 public class Main {
 	
-	enum Level {
-		HIGHT,
-		LOW
-	}
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello \n");
-		System.out.println(Level.HIGHT);
+		
+		int num = fattoriale (5) ;
+		System.out.println(num);
+		
 
 	}
+	
+	public static int fattoriale(int n){
+	    if(n < 0)
+	        return -1;
+	    if(n == 0)
+	        return 1;
+	    return n * fattoriale(n-1);
+	}
 
+
+    
+    
 }
