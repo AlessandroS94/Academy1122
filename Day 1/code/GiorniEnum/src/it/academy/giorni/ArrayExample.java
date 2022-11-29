28d34430756e416e36957064d953af7f3a7a099e
Alessandro <alex_994@ymail.com>
2022-11-29 08:36:25 +0100

    update

---
+++ Day 1/code/GiorniEnum/src/it/academy/giorni/ArrayExample.java
@@ -0,0 +1,21 @@
+package it.academy.giorni;
+
+public class ArrayExample {
+    public static void main(String[] args) {
+        String [] nomi = {"Lunedì", "Martedì","Mercoledì","Giovedì","Venderdì","Sabato","Domenica"};
+        System.out.println("Stampa con ciclo for");
+        for(int i=0; i < nomi.length; i++){
+            System.out.println(nomi[i]);
+        }
+        System.out.println("Stampa con ciclo for-each");
+        for (String s: nomi){
+            System.out.println(s);
+        }
+        System.out.println("Stampa con ciclo while");
+        int i = 0;
+        while (i< nomi.length){
+            System.out.println(nomi[i]);
+            i++;
+        }
+    }
+}
---
+++ Day 1/code/SimpleExample/src/main/java/it/academy/corso/SimpleExampleApplication.java
@@ -0,0 +1,13 @@
+package it.academy.corso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleExampleApplication.class, args);
+	}
+
+}
